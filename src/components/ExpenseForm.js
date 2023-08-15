5321db0e5af6c8ed8e5373af26f1b47e31e08b9c
Sreenivasulu Malinedi <sreenivasulumalinedi@Sreenivasulus-MacBook-Pro.local>
2023-08-15 10:57:44 -0500

    Expense form validation added

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = (props) => {
@@ -55,15 +55,49 @@ const ExpenseForm = (props) => {
   //   console.log(userInput.title);
   // };
 
+  const [validTtile, setValidTitle] = useState(true);
+  const [validAmount, setValidAmount] = useState(true);
+  const [validDate, setValidDate] = useState(true);
+
   const constructExpence = (event) => {
     event.preventDefault();
 
-    const selectedDate = date.split("-");
+    const [month, day, year] = date.split("-");
+    var pattern = /^([0-9]{4})-([0-9]{2})-([0-9]{2})$/;
+
+    if (title.trim().length == 0) {
+      console.log("Tirle Error");
+      setValidTitle(false);
+    } else {
+      setValidTitle(true);
+    }
+
+    if (amount.trim().length == 0) {
+      console.log("Amt Error");
+      setValidAmount(false);
+    } else {
+      setValidAmount(true);
+    }
+
+    if (!pattern.test(date)) {
+      console.log("Date Error");
+      setValidDate(false);
+    } else {
+      setValidDate(true);
+    }
+
+    if (
+      title.trim().length == 0 ||
+      amount.trim().length == 0 ||
+      !pattern.test(date)
+    ) {
+      return;
+    }
 
     const newExpense = {
       desc: title,
       price: amount,
-      date: new Date(selectedDate[0], selectedDate[1] - 1, selectedDate[2]),
+      date: new Date(month, day - 1, year),
     };
     setTitle("");
     setAmount("");
@@ -74,11 +108,21 @@ const ExpenseForm = (props) => {
   return (
     <form onSubmit={constructExpence}>
       <div className="new-expense__controls">
-        <div className="new-expense__control">
+        <div
+          className={
+            validTtile ? "new-expense__control" : "new-expense__control invalid"
+          }
+        >
           <label> Title </label>
           <input type="text" value={title} source="title" onChange={setValue} />
         </div>
-        <div className="new-expense__control">
+        <div
+          className={
+            validAmount
+              ? "new-expense__control"
+              : "new-expense__control invalid"
+          }
+        >
           <label> Amount </label>
           <input
             type="number"
@@ -89,7 +133,11 @@ const ExpenseForm = (props) => {
             onChange={setValue}
           />
         </div>
-        <div className="new-expense__control">
+        <div
+          className={
+            validDate ? "new-expense__control" : "new-expense__control invalid"
+          }
+        >
           <label> Date </label>
           <input type="date" value={date} source="date" onChange={setValue} />
         </div>
